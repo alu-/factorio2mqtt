fdf63cbd1a0b0d9f538cb6019a5c53f365f33ca9
alu- <alu@byteberry.net>
2024-11-30 22:58:50 +0100

    Add node publisher

--- src/server/index.js
+++ src/server/index.js
@@ -1,46 +1,73 @@
 import chokidar from 'chokidar';
 import fs from "fs";
-import {basename} from "path";
+import { basename, resolve } from "path";
 import mqtt from "mqtt";
+import { createLogger, LogContexts, LogLevels } from 'bs-logger';
 
-const client = mqtt.connect("mqtt://test.mosquitto.org");
+const logger = createLogger({ 
+    [LogContexts.application]: 'factorio2mqtt',
+    targets: "stdout:" + (process.env.LOG_TARGETS || LogLevels.info)
+});
 
 const args = process.argv.slice(2);
 if (args.length === 0) {
-    console.log("Please run with path to script-output directory.");
+    logger.fatal("Please run with path to script-output directory.");
     process.exit(1);
 }
 
 if (!fs.existsSync(args[0])) {
-    console.log("Provided path doesn't exist.");
+    logger.fatal("Provided path to script-output doesn't exist.");
     process.exit(1);
 }
 
-async function handle(path)
-{
-    fs.readFile(path, function(err, data) {
+// @todo handle files containing faulty json, they might not have been fully written yet
+// @todo test slow writes
+async function handle(path) {
+    fs.readFile(path, function (err, data) {
         if (err) throw err;
 
         const transaction = JSON.parse(data);
-        console.log(transaction);
-        client.publish("factorio", JSON.stringify(transaction));
+        client.publish(
+            "factorio/" + transaction.name, 
+            JSON.stringify(transaction)
+        );
+
+        fs.unlink(path, (err) => {
+            if (err) throw err;
+            logger.debug("Unlinked: " + path);
+        });
     });
 
 }
+logger("Connecting to MQTT");
+const client = mqtt.connect(process.env.MQTT_HOST, {
+    "username": process.env.MQTT_USERNAME,
+    "password": process.env.MQTT_PASSWORD,
+});
 
-chokidar
-    .watch(
-        args[0],
-        {
-            depth: 0,
-            awaitWriteFinish: true
-        })
-    .on('add', (path) => {
-        const filename = basename(path);
-        if (filename.startsWith('factorio2mqtt_')) {
-            console.log(path);
-            handle(path);
-        }
-    }
-);
-
+client.on("connect", () => {
+    logger("Connected to MQTT server");
+    chokidar
+        .watch(
+            args[0],
+            {
+                persistent: true,
+                depth: 0,
+                //awaitWriteFinish: true // @todo test if this is needed with Factorio..
+                followSymlinks: false,
+                alwaysStat: false,
+            })
+        .on('add', (path) => {
+            logger.debug("Found new file", path);
+            const filename = basename(path);
+            if (filename.startsWith('factorio2mqtt_')) {
+                handle(path);
+            }
+        });
+})
+    .on("reconnect", () => {
+        logger.info("Reconnecting to MQTT")
+    })
+    .on("error", function (error) {
+        console.info("Error with MQTT: ", error);
+    });
